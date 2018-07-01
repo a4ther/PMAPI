3dc1ee05bbd06a3714094e5327ea98d6a0f474c9
Lee <leealfsoto@gmail.com>
2018-07-01 16:02:32 -0600

    Added Categories implementation to API

---
+++ PM.API/Infrastructure/Validators/PutCategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PM.API.Models.Request;
+
+namespace PM.API.Infrastructure.Validators
+{
+    public class PutCategoryValidator : AbstractValidator<PutCategory>
+    {
+        public PutCategoryValidator()
+        {
+            RuleFor(c => c.ID).NotEmpty();
+            RuleFor(c => c.Name).NotEmpty().Length(1, 25);
+        }
+    }
+}
